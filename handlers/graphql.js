82526d95146966b4ccab594635737695807e66b3
Paul Dariye <paul.dariye@gmail.com>
2019-10-21 08:16:22 +0200

    Ensure get resolver return type match GQL schema

--- handlers/graphql.js
+++ handlers/graphql.js
@@ -27,7 +27,21 @@ const resolvers = {
   Query: {
     async get(_parent, { isbn }, _context) {
       if (validator.isISBN(isbn)) {
-        return find(isbn);
+        const data = await find(isbn);
+        return {
+          id: data.id,
+          url: data.url,
+          title: data.title,
+          isbn10: data.isbn_10,
+          isbn13: data.isbn_13,
+          author: data.author,
+          publisher: data.publisher,
+          published: data.published,
+          source: data.source,
+          language: data.language,
+          binding: data.binding,
+          cover: data.cover
+        };
       } else {
         throw new UserInputError(`Invalid ISBN: ${isbn}`);
       }
