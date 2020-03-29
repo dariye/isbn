98299211d95ba4aca23e2ae632dc63d053c7c111
paul <paul.dariye@gmail.com>
2020-03-29 18:16:11 +0200

    Fix typo

--- sources/index.js
+++ sources/index.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 const path = require("path");
 
 const normalizedPath = path.join(__dirname, ".");
-const blacklist = ["index.js", "d.js", "e.js", "d.js"];
+const blacklist = ["index.js", "d.js", "e.js", "f.js"];
 
 fs.readdirSync(normalizedPath).forEach(function(file) {
   if (!blacklist.includes(file)) {
