5fc270a54f7749d179894c5e8b6cb21d395890a1
paul <paul.dariye@gmail.com>
2021-01-06 00:26:30 +0100

    Rename blacklist to omit

--- sources/index.js
+++ sources/index.js
@@ -2,10 +2,10 @@ const fs = require("fs");
 const path = require("path");
 
 const normalizedPath = path.join(__dirname, ".");
-const blacklist = ["index.js", "d.js", "e.js", "f.js", "g.js"];
+const omit = ["index.js", "d.js", "e.js", "f.js", "g.js"];
 
 fs.readdirSync(normalizedPath).forEach(function(file) {
-  if (!blacklist.includes(file)) {
+  if (!omit.includes(file)) {
     const name = file.replace(".js", "");
     exports[name] = require(`${normalizedPath}/${file}`);
   }
